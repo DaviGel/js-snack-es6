d1c33eacebd92819ba7a273d46a8cc528aca9671
DaviGel <geloso.davide@gmail.com>
2023-11-22 16:52:30 +0100

    feat: snack 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,57 @@
 'use strict';
+
+/* Functions */
+
+/* Program */
+
+// Snack 1
+
+const bici = [
+  {
+    nome: 'Graziella',
+    peso: 30,
+  },
+  {
+    nome: 'Ludovica',
+    peso: 12,
+  },
+  {
+    nome: 'Rossana',
+    peso: 32,
+  },
+  {
+    nome: 'Marta',
+    peso: 45,
+  },
+  {
+    nome: 'Francesca',
+    peso: 70,
+  },
+  {
+    nome: 'Rossella',
+    peso: 67,
+  },
+  {
+    nome: 'Elisabetta',
+    peso: 87,
+  },
+];
+
+const container = document.createElement('div');
+container.classList.add('container');
+const body = document.querySelector('body');
+body.append(container);
+
+let tempWeight = 0;
+let tempName = '';
+
+bici.forEach((bicicletta, index) => {
+  const { nome, peso } = bicicletta;
+  if (index === 0) tempWeight = peso;
+  else if (tempWeight > peso) {
+    tempName = nome;
+    tempWeight = peso;
+  }
+});
+
+container.innerHTML = `<span>La bici di nome: "${tempName}" pesa ${tempWeight}kg</span>`;
